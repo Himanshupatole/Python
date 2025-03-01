af00f8b318687952e9cce142eee2bc655ce05209
Himanshupatole <143797761+Himanshupatole@users.noreply.github.com>
2025-03-01 23:53:30 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -2,4 +2,4 @@ def is_anagram(str1, str2):
     return sorted(str1) == sorted(str2)
 
 print(is_anagram("listen", "silent"))  # True
-print(is_anagram("hello", "world"))   # False
+print(is_anagram("hello", "world"))   
