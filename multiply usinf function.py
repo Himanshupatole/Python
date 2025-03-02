cb86d9bd8906af593c3bbb6554a217868556cce5
Himanshupatole <143797761+Himanshupatole@users.noreply.github.com>
2025-03-02 23:53:34 +0530

    Add files via upload

---
+++ multiply usinf function.py
@@ -0,0 +1,10 @@
+def multiply(a, b):
+    return a * b
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calling the function and displaying the result
+result = multiply(num1, num2)
+print("Multiplication Result:", result)
